ab47c8a4685e182c8615b7e02e666a547b52f54e
Aryan <aryanverma170372@gmail.com>
2023-02-28 02:36:09 +0530

    code:GetVillas function added

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -1,9 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MagicVilla_VillaAPI.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla_VillaAPI.Controllers
 {
+
     [ApiController]
     public class VillaAPIController :ControllerBase
     {
+        public IEnumerable<Villa> GetVillas() //Creating a public enumerable method getVillas of type Villa 
+        {
+            return new List<Villa> //Creating a list of type Villa
+            {
+                new Villa {Id=1,Name="Pool View"},
+                new Villa {Id=2,Name="Beach View"}
+            };
+        }
     }
 }
