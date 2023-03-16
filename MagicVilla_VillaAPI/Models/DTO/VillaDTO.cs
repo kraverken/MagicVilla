374b180914d95ea2bebf9455441557eaf4a7717c
Aryan <aryanverma170372@gmail.com>
2023-03-16 17:34:19 +0530

    code: Adding validations to ModalState

--- MagicVilla_VillaAPI/Models/DTO/VillaDTO.cs
+++ MagicVilla_VillaAPI/Models/DTO/VillaDTO.cs
@@ -1,8 +1,13 @@
-﻿namespace MagicVilla_VillaAPI.Models.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagicVilla_VillaAPI.Models.DTO
 {
     public class VillaDTO
     {
         public int Id { get; set; }
+        [Required]//adding data annotations on modal class thru .net for field name 
+        //These validations are provided by the [APIController] and if we comment it out then these validations will be of no use
+        [MaxLength(30)]
         public string Name { get; set; }
     }
 }
