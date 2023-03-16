acf1ccb915a8f8f0b32c35f0f1067771b8f327a1
Aryan <aryanverma170372@gmail.com>
2023-03-16 22:53:33 +0530

    Note: How to add XML o/p support

--- MagicVilla_VillaAPI/Program.cs
+++ MagicVilla_VillaAPI/Program.cs
@@ -2,7 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers().AddNewtonsoftJson();
+builder.Services.AddControllers().AddNewtonsoftJson();//.AddXmlDataContractSerializerFormatters() in order to add XML output format support 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
